e0dde6fd0094faa277f088ab4918e7c2927301a2
daeseungmoon <tmdeoans@snu,ac,jr>
2023-01-09 14:19:34 +0900

    svelte로 storybook 짜는거 포기

--- packages/uglychart/src/lib/chart/bar/BarChart.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  import BarCharWidget from "./BarChartWidget"
-  import Widget from "@moonmoonbrothers/like-flutter-svelte"
-</script>
-
-<Widget height="calc(100vh - 60px)" widget={new BarCharWidget()} />
