a3f2d477907ce853f8c7ab962b6fd636c18eca62
daeseungmoon <tmdeoans@snu,ac,jr>
2023-01-09 14:18:12 +0900

    Revert "svelte로 previewjs 보는거 포기"
    This reverts commit f0671f5867c0449ad269dd0a1a445a97843066d7.

---
+++ packages/uglychart/src/lib/chart/bar/BarChart.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import BarCharWidget from "./BarChartWidget"
+  import Widget from "@moonmoonbrothers/like-flutter-svelte"
+</script>
+
+<Widget height="calc(100vh - 60px)" widget={new BarCharWidget()} />
