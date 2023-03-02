d27dd8cc5aaf4047fc5ad52e424be817edfca27e
daeseungmoon <tmdeoans@snu.ac.kr>
2023-03-02 11:05:50 +0900

    refactor render object paint

--- packages/flutterjs/src/renderobject/RenderObject.ts
+++ packages/flutterjs/src/renderobject/RenderObject.ts
@@ -1,109 +1,113 @@
-import type RenderObjectElement from "../element/RenderObjectElement"
-import { Size, Offset, Constraint } from "../type"
-import type { PaintContext } from "../utils/type"
-import ShortUniqueId from "short-unique-id"
+import type RenderObjectElement from "../element/RenderObjectElement";
+import { Size, Offset, Constraint } from "../type";
+import type { PaintContext } from "../utils/type";
+import ShortUniqueId from "short-unique-id";
 
-const uid = new ShortUniqueId({ dictionary: "hex" })
+const uid = new ShortUniqueId({ dictionary: "hex" });
 
 class RenderObject {
-  isPainter: boolean
-  id = uid.randomUUID(6)
-  ownerElement!: RenderObjectElement
+  isPainter: boolean;
+  id = uid.randomUUID(6);
+  ownerElement!: RenderObjectElement;
   constructor({ isPainter }: { isPainter: boolean }) {
-    this.isPainter = isPainter
+    this.isPainter = isPainter;
   }
-  type = this.constructor.name
+  type = this.constructor.name;
   get children(): RenderObject[] {
-    return this.ownerElement.children.map((child) => child.renderObject)
+    return this.ownerElement.children.map((child) => child.renderObject);
   }
-  size: Size = Size.zero()
-  constraint: Constraint = Constraint.loose(Size.maximum())
-  offset: Offset = Offset.zero()
+  size: Size = Size.zero();
+  constraint: Constraint = Constraint.loose(Size.maximum());
+  offset: Offset = Offset.zero();
 
   layout(constraint: Constraint) {
-    this.constraint = constraint.normalize()
-    this.preformLayout()
+    this.constraint = constraint.normalize();
+    this.preformLayout();
   }
 
   paint(context: PaintContext, offset: Offset) {
-    const totalOffset = offset.plus(this.offset)
+    const totalOffset = offset.plus(this.offset);
     if (this.isPainter) {
-      const svgEls = this.findOrAppendSvgEl(context)
-      Object.values(svgEls).forEach((svgEl) =>
-        svgEl.setAttribute(
-          "transform",
-          `translate(${totalOffset.x} ${totalOffset.y})`
-        )
-      )
-      this.performPaint(svgEls)
+      // this line should be refactored.. It always return only one svgEl.
+      const { svgEls, container } = this.findOrAppendSvgEl(context);
+      container.setAttribute(
+        "transform",
+        `translate(${totalOffset.x} ${totalOffset.y})`
+      );
+      this.performPaint(svgEls);
     }
-    this.children.forEach((child) => child.paint(context, totalOffset))
+    this.children.forEach((child) => child.paint(context, totalOffset));
   }
 
   attach(ownerElement: RenderObjectElement) {
-    this.ownerElement = ownerElement
+    this.ownerElement = ownerElement;
   }
 
   dispose(context: PaintContext) {
     if (this.isPainter) {
-      context.findSvgEl(this.id)?.remove()
+      context.findSvgEl(this.id)?.remove();
     }
-    this.children.forEach((child) => child.dispose(context))
+    this.children.forEach((child) => child.dispose(context));
   }
 
   getIntrinsicWidth() {
-    return 0
+    return 0;
   }
 
   getIntrinsicHeight() {
-    return 0
+    return 0;
   }
 
   private findOrAppendSvgEl(context: PaintContext) {
-    const { findSvgEl, appendSvgEl } = context
-    const oldEl = findSvgEl(this.id)
-    let svgEls: { [key: string]: SVGElement } = {}
+    const { findSvgEl, appendSvgEl } = context;
+    const oldEl = findSvgEl(this.id);
+    let svgEls: { [key: string]: SVGElement } = {};
+    let container: SVGElement;
     if (oldEl) {
+      container = oldEl;
       if (oldEl.nodeName === "g") {
         for (const child of oldEl.children) {
           // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          const name = child.getAttribute("data-render-name")!
-          svgEls[name] = child as unknown as SVGElement
+          const name = child.getAttribute("data-render-name")!;
+          svgEls[name] = child as unknown as SVGElement;
         }
       } else {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const name = oldEl.getAttribute("data-render-name")!
-        svgEls[name] = oldEl
+        const name = oldEl.getAttribute("data-render-name")!;
+        svgEls[name] = oldEl;
       }
     } else {
-      svgEls = this.createDefaultSvgEl(context)
+      svgEls = this.createDefaultSvgEl(context);
       Object.entries(svgEls).forEach(([name, value]) => {
-        value.setAttribute("data-render-name", name)
-      })
-      const values = Object.values(svgEls)
+        value.setAttribute("data-render-name", name);
+      });
+      const values = Object.values(svgEls);
+      // For Optimization, only one svg element don't need to be wraped <g></g>.
       if (values.length === 1) {
-        const svgEl = values[0]
-        context.setId(svgEl, this.id)
-        svgEl.setAttribute("data-render-type", this.type)
-        appendSvgEl(svgEl)
+        const svgEl = values[0];
+        container = svgEl;
+        context.setId(svgEl, this.id);
+        svgEl.setAttribute("data-render-type", this.type);
+        appendSvgEl(svgEl);
       } else {
-        const svgG = context.createSvgEl("g")
-        context.setId(svgG, this.id)
-        appendSvgEl(svgG)
-        svgG.setAttribute("data-render-type", this.type)
-        values.forEach(value => {
-          svgG.appendChild(value)
-        })
+        const svgG = context.createSvgEl("g");
+        container = svgG;
+        context.setId(svgG, this.id);
+        appendSvgEl(svgG);
+        svgG.setAttribute("data-render-type", this.type);
+        values.forEach((value) => {
+          svgG.appendChild(value);
+        });
       }
     }
-    return svgEls
+    return { svgEls, container };
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected createDefaultSvgEl(paintContext: PaintContext): {
-    [key: string]: SVGElement
+    [key: string]: SVGElement;
   } {
-    throw { message: "not implemented defaultSvgEl" }
+    throw { message: "not implemented defaultSvgEl" };
   }
 
   /*
@@ -111,7 +115,7 @@ class RenderObject {
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   protected preformLayout(): void {
-    throw { message: "not implemented performLayout" }
+    throw { message: "not implemented performLayout" };
   }
 
   /*
@@ -121,4 +125,4 @@ class RenderObject {
   protected performPaint(svgEls: { [key: string]: SVGElement }): void {}
 }
 
-export default RenderObject
+export default RenderObject;
--- packages/storybook-flutterjs/src/routes/+page.svelte
+++ packages/storybook-flutterjs/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+
+</script>
+
+
+<svg viewBox="0 0 200 200">
+   <!-- 클립패스 요소 -->
+  <clipPath id="circle">
+	  <circle cx="25" cy="25" r="25" />  
+  </clipPath>
+  <clipPath id="with-rect" clip-path="url(#circle)">
+	  <rect x="25" y="25" width="25" height="25" />  
+  </clipPath>
+  <clipPath id="clippath" clip-path="url(#with-rect)" style="transform: translateX(-10px);">
+	  <rect x="35" y="25" width="15" height="10" />  
+  </clipPath>
+  <rect clip-path="url(#clippath)" on:click={() => alert('clicked!!')} fill="yellow" x="0" y="0" width="50" height="50" />
+
+</svg>
