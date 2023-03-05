db15b3fc96445ab2b593d9f8f4f9833208393a61
daeseungmoon <tmdeoans@snu.ac.kr>
2023-03-05 17:55:29 +0900

    asdf

--- packages/storybook-flutterjs/src/stories/Container/Container.stories.ts
+++ packages/storybook-flutterjs/src/stories/Container/Container.stories.ts
@@ -1,80 +1,99 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Meta, StoryObj } from '@storybook/svelte';
 import Widget from '../../Widget.svelte';
 import { Container, Text, Alignment, EdgeInsets } from '@moonmoonbrothers/flutterjs';
+import { dedent } from 'ts-dedent';
+const ImportWidgetCode = dedent`import { Container, Text, Alignment, EdgeInsets } from '@moonmoonbrothers/flutterjs';
+\n\n`;
 
 const meta = {
 	title: 'Widget/Container',
 	component: Widget,
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
 	tags: ['autodocs'],
 	parameters: {
-		// More on how to position stories at: https://storybook.js.org/docs/7.0/svelte/configure/story-layout
-		layout: 'fullscreen'
+		docs: {
+			description: {
+				component: dedent`
+					This is Container widget. 
+					## Hi
+				`
+			}
+		}
 	}
 } satisfies Meta<Widget>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const Case1Code = dedent`
+	Container({
+		color: 'lightblue'
+	})
+`;
 export const Case1: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightblue'
-		})
-		// description: 'if container has no child, it would be stretched as possibe as it can be'
+		widget: eval(Case1Code),
+		code: ImportWidgetCode + Case1Code
 	}
 };
+const Case2Code = dedent`
+	Container({
+		color: 'lightblue',
+		child: Text('text', { style: { fontSize: '30px' } })
+	})
+`;
 export const Case2: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightblue',
-			child: Text('text', { style: { fontSize: '30px' } })
-		})
-		// description: 'if container has child, it would be shirinked in order to fit child'
+		widget: eval(Case2Code),
+		code: ImportWidgetCode + Case2Code
 	}
 };
+const Case3Code = dedent`
+	Container({
+		color: 'lightblue',
+		width: 300,
+		height: 300,
+		padding: EdgeInsets.all(10),
+		child: Container({
+			color: 'green',
+			child: Text('child in blue container')
+		})
+	})
+`;
 export const Case3: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightblue',
-			width: 300,
-			height: 300,
-			padding: EdgeInsets.all(10),
-			child: Container({
-				color: 'green',
-				child: Text('child in blue container')
-			})
-		})
-		// description: `if container has width and height,
-		// it pass tight constraint that means any child in this component would be stretched as possible as it can be`
+		widget: eval(Case3Code),
+		code: ImportWidgetCode + Case3Code
 	}
 };
+const Case4Code = dedent`
+	Container({
+		color: 'lightblue',
+		width: 300,
+		height: 300,
+		padding: EdgeInsets.all(10),
+		alignment: Alignment.center,
+		child: Container({
+			color: 'green',
+			child: Text('child')
+		})
+	})
+`;
 export const Case4: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightblue',
-			width: 300,
-			height: 300,
-			padding: EdgeInsets.all(10),
-			alignment: Alignment.center,
-			child: Container({
-				color: 'green',
-				child: Text('child')
-			})
-		})
-		// 	description: `although the container is in tight constraint(including that it have own width and height),
-		//  containter with alignment loosen constraint. so child dont have to be expanded`
+		widget: eval(Case4Code),
+		code: ImportWidgetCode + Case4Code
 	}
 };
--- packages/storybook-flutterjs/src/stories/Stack/Stack.stories.ts
+++ packages/storybook-flutterjs/src/stories/Stack/Stack.stories.ts
@@ -1,82 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Meta, StoryObj } from '@storybook/svelte';
 import Widget from '../../Widget.svelte';
 import { Container, Alignment, Stack, Positioned } from '@moonmoonbrothers/flutterjs';
+import { dedent } from 'ts-dedent';
+const ImportWidgetCode = dedent`import { Container, Alignment, Stack, Positioned } from '@moonmoonbrothers/flutterjs';
+\n\n`;
 
 const meta = {
 	title: 'Widget/Stack',
 	component: Widget,
-	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/7.0/react/writing-docs/docs-page
 	tags: ['autodocs'],
 	parameters: {
-		// More on how to position stories at: https://storybook.js.org/docs/7.0/svelte/configure/story-layout
-		layout: 'fullscreen'
+		docs: {
+			description: {
+				component: dedent`
+					This is Stack widget. 
+					## Hi
+				`
+			}
+		}
 	}
 } satisfies Meta<Widget>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const Case1Code = dedent`
+	Container({
+		color: 'lightgrey',
+		alignment: Alignment.center,
+		child: Stack({
+			children: [
+				Container({
+					width: 100,
+					height: 100,
+					color: 'green'
+				}),
+				Container({
+					width: 50,
+					height: 50,
+					color: 'red'
+				})
+			]
+		})
+	})
+`;
 export const Case1: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightgrey',
-			alignment: Alignment.center,
-			child: Stack({
-				children: [
-					Container({
-						width: 100,
-						height: 100,
-						color: 'green'
-					}),
-					Container({
+		widget: eval(Case1Code),
+		code: ImportWidgetCode + Case1Code
+	}
+};
+
+const Case2Code = dedent`
+	Container({
+		color: 'lightgrey',
+		width: Infinity,
+		height: Infinity,
+		alignment: Alignment.center,
+		child: Stack({
+			children: [
+				Container({
+					width: 100,
+					height: 100,
+					color: 'green'
+				}),
+				Positioned({
+					bottom: 0,
+					right: 0,
+					child: Container({
 						width: 50,
 						height: 50,
 						color: 'red'
 					})
-				]
-			})
+				})
+			]
 		})
-		// description: `
-		//   Stack try to fit maximum child size
-		// `
-	}
-};
+	})
+`;
 
 export const Case2: Story = {
 	args: {
 		ssrSize: { width: 600, height: 300 },
 		width: '600px',
 		height: '300px',
-		widget: Container({
-			color: 'lightgrey',
-			width: Infinity,
-			height: Infinity,
-			alignment: Alignment.center,
-			child: Stack({
-				children: [
-					Container({
-						width: 100,
-						height: 100,
-						color: 'green'
-					}),
-					Positioned({
-						bottom: 0,
-						right: 0,
-						child: Container({
-							width: 50,
-							height: 50,
-							color: 'red'
-						})
-					})
-				]
-			})
-		})
-		// description: `
-		//   Positioned widget in Stack can decide to its own offset with props: top, left, right, bottom,
-		//   here is red one with bttom: 0, right: 0
-		// `
+		widget: eval(Case2Code),
+		code: ImportWidgetCode + Case2Code
 	}
 };
