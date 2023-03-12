e6daaf0a2815120abd99f7db32b4a69ddae74757
daeseungmoon <tmdeoans@snu.ac.kr>
2023-03-12 10:05:27 +0900

    Center 추가

--- packages/flutterjs/src/component/Center.ts
+++ packages/flutterjs/src/component/Center.ts
@@ -4,17 +4,17 @@ import Align from "./base/BaseAlign"
 
 export default function Center({
   child,
-  width = Infinity,
-  height = Infinity,
+  widthFactor,
+  heightFactor,
 }: {
   child?: Widget
-  width?: number
-  height?: number
+  widthFactor?: number
+  heightFactor?: number
 }) {
   return new Align({
     child,
-    widthFactor: width,
-    heightFactor: height,
+    widthFactor: widthFactor,
+    heightFactor: heightFactor,
     alignment: Alignment.center,
   })
 }
