47887a279b64fee34babf8b521e70d7f9f9656e6
daeseungmoon <tmdeoans@snu.ac.kr>
2023-03-20 17:57:58 +0900

    vector4 수정

--- packages/flutterjs/src/type/_types/Vector2.ts
+++ packages/flutterjs/src/type/_types/Vector2.ts
@@ -3,6 +3,9 @@ import Vector from "./Vector";
 export class Vector2 implements Vector {
   _v2storage: [number, number]
 
+  constructor(arg0: number, arg1: number,) {
+    this._v2storage = [arg0, arg1];
+  }
 }
 
 export default Vector2
--- packages/flutterjs/src/type/_types/Vector3.ts
+++ packages/flutterjs/src/type/_types/Vector3.ts
@@ -2,6 +2,10 @@ import Vector from "./Vector";
 
 export class Vector3 implements Vector {
   _v3storage: [number, number, number]
+
+  constructor(arg0: number, arg1: number, arg2: number) {
+    this._v3storage = [arg0, arg1, arg2];
+  }
 }
 
 export default Vector3
