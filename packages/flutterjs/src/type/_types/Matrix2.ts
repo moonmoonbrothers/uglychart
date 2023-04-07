151e5f33b2ab78950519c69b14d615abe54d49be
daeseungmoon <tmdeoans@snu.ac.kr>
2023-04-07 11:31:59 +0900

    futterjs, flutter-svelte npm 배포

--- packages/flutterjs/src/type/_types/Matrix2.ts
+++ packages/flutterjs/src/type/_types/Matrix2.ts
@@ -1,15 +1,14 @@
 class Matrix2 {
   get storage() {
-    return this._m2storage
+    return this._m2storage;
   }
-  _m2storage: Array9
+  _m2storage: Array4;
+
+  constructor(arg1: number, arg2: number, arg3: number, arg4: number) {
+    this._m2storage = [arg1, arg2, arg3, arg4];
+  }
 }
 
-export default Matrix2
+export default Matrix2;
 
-type Array9 = [
-  number,
-  number,
-  number,
-  number,
-];
+type Array4 = [number, number, number, number];
