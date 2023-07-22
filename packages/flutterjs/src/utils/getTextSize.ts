2800a8dc50eed45df7a26a3e257d585a6650d12c
daeseungmoon <tmdeoans@snu.ac.kr>
2023-07-22 15:20:31 +0900

    humm....

--- packages/flutterjs/src/utils/getTextSize.ts
+++ packages/flutterjs/src/utils/getTextSize.ts
@@ -1,14 +1,38 @@
-import { createCanvas } from "canvas";
-const canvas = createCanvas(0, 0);
-const ctx = canvas.getContext("2d");
+import pixelWidth from "string-pixel-width";
+const OFFSET = 20;
+const SCALE = 100;
+const defaultWidthMapStr = `007LLmW'55;N0500LLLLLLLLLL00NNNLzWW\\\\WQb\\0FWLg\\bWb\\WQ\\WrWWQ000CL5LLFLL0LL**F*gLLLL5F0LF\\FFF5.5N`;
+export const DEFAULT_FONT_SIZE = 12;
+export const DEFAULT_FONT_FAMILY = "sans-serif";
+export const DEFAULT_FONT = `${DEFAULT_FONT_SIZE}px ${DEFAULT_FONT_FAMILY}`;
 
-function getCtx() {
-  // brouswer에서는 다른걸 써야 할지도..?
-  if (typeof window !== "undefined") {
-    return ctx;
-  } else {
-    return ctx;
+function getTextWidthMap(mapStr: string): Record<string, number> {
+  const map: Record<string, number> = {};
+  if (typeof JSON === "undefined") {
+    return map;
   }
+  for (let i = 0; i < mapStr.length; i++) {
+    const char = String.fromCharCode(i + 32);
+    const size = (mapStr.charCodeAt(i) - OFFSET) / SCALE;
+    map[char] = size;
+  }
+  return map;
+}
+
+export const DEFAULT_TEXT_WIDTH_MAP = getTextWidthMap(defaultWidthMapStr);
+
+let _ctx: CanvasRenderingContext2D;
+
+function getCtxOrNull(): CanvasRenderingContext2D | null {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  if (_ctx == null) {
+    _ctx = document.createElement("canvas").getContext("2d");
+  }
+
+  return _ctx;
 }
 
 export function getTextWidth({
@@ -18,15 +42,36 @@ export function getTextWidth({
   text: string;
   font: string;
 }): number {
-  const ctx = getCtx();
-  ctx.font = font;
-  return Math.ceil(ctx.measureText(text).width);
+  const ctx = getCtxOrNull();
+  if (ctx != null) {
+    ctx.font = font;
+    const width = Math.ceil(ctx.measureText(text).width);
+    console.log("client", ctx.measureText(text).width);
+    return width;
+  }
+
+  // Use font size if there is no other method can be used.
+  const res = /(\d+)px/.exec(font);
+  const fontSize = (res && +res[1]) || DEFAULT_FONT_SIZE;
+  let width = 0;
+  if (font.indexOf("mono") >= 0) {
+    // is monospace
+    width = fontSize * text.length;
+  } else {
+    for (let i = 0; i < text.length; i++) {
+      const preCalcWidth = DEFAULT_TEXT_WIDTH_MAP[text[i]];
+      width += preCalcWidth == null ? fontSize : preCalcWidth * fontSize;
+    }
+  }
+
+  console.log("node", pixelWidth(text, { font: "open sans", size: 16 }));
+
+  return Math.ceil(width);
 }
 /*
  * Calculate height of canvas text
  * https://developer.mozilla.org/en-US/docs/Web/API/TextMetrics
  * */
-
 export function getTextHeight({ font }: { text: string; font: string }) {
   return getFontHeight(font);
 }
