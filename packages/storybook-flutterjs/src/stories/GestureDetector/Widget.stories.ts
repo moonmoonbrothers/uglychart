99db3bd4d679f7ee8a879ad545598033b588f383
daeseungmoon <tmdeoans@snu.ac.kr>
2023-07-27 18:39:10 +0900

    test case 작성

--- packages/storybook-flutterjs/src/stories/GestureDetector/Widget.stories.ts
+++ packages/storybook-flutterjs/src/stories/GestureDetector/Widget.stories.ts
@@ -5,7 +5,9 @@ import {
 	Container,
 	GestureDetector,
 	Text,
-	TextStyle
+	TextStyle,
+	ComponentWidget,
+	BuildContext
 } from '@moonmoonbrothers/flutterjs';
 import { dedent } from 'ts-dedent';
 
@@ -63,5 +65,50 @@ export const Basic: Story = {
 				})
 			})
 		})`
+	}
+};
+
+class CustomWidget extends ComponentWidget {
+	colors: string[];
+	index = 0;
+	constructor({ colors, key }: { colors: string[]; key?: string }) {
+		super(key);
+		this.colors = colors;
+	}
+	build(context: BuildContext) {
+		const handleClick = () => {
+			this.index = this.index + 1 + this.colors.length;
+		};
+		return GestureDetector({
+			onClick() {
+				handleClick();
+			},
+			child: Container({
+				width: 200,
+				height: 200,
+				color: this.colors[this.index],
+				alignment: Alignment.center,
+				child: Text('click here!!', {
+					style: new TextStyle({ fontSize: 20, fontWeight: 'bold', color: 'white' })
+				})
+			})
+		});
+	}
+}
+
+export const ColorChange: Story = {
+	args: {
+		width: '400px',
+		height: '400px',
+		ssrSize: { width: 400, height: 400 },
+		widget: Container({
+			alignment: Alignment.center,
+			color: 'lightgreen',
+			child: new CustomWidget({
+				colors: ['black', 'red', 'green']
+			})
+		}),
+		code: dedent`
+		`
 	}
 };
