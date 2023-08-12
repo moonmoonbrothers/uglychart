6de5bf727e02f7efc2c67264eb16cc6e18b307d5
daeseungmoon <tmdeoans@snu.ac.kr>
2023-08-12 23:31:45 +0900

    continuing...

--- packages/flutterjs/src/provider/ProviderWidget.ts
+++ packages/flutterjs/src/provider/ProviderWidget.ts
@@ -1,79 +1,81 @@
-import Element from "../element/Element"
-import Widget from "../widget/Widget"
+import typia from "typia";
+import Element from "../element/Element";
+import Widget from "../widget/Widget";
 
 class ProviderWidget<ProviderKey, Value> extends Widget {
-  providerKey: ProviderKey
-  value: Value
-  child: Widget
+  providerKey: ProviderKey;
+  value: Value;
+  child: Widget;
   constructor({
     child,
     providerKey,
     value,
   }: {
-    child: Widget
-    providerKey: ProviderKey
-    value: Value
+    child: Widget;
+    providerKey: ProviderKey;
+    value: Value;
   }) {
-    super()
-    this.child = child
-    this.providerKey = providerKey
-    this.value = value
+    super();
+    this.child = child;
+    this.providerKey = providerKey;
+    this.value = value;
   }
 
   static of<V>(key: unknown, context: Element) {
-    let parent = context.parent
+    let parent = context.parent;
     while (parent != null) {
-      const current = parent
-      parent = current.parent
-      if (!(current instanceof ProviderElement)) continue
-      if (current.providerKey !== key) continue
+      const current = parent;
+      parent = current.parent;
+      if (!(current instanceof ProviderElement)) continue;
+      if (current.providerKey !== key) continue;
+      console.log(key, typia.is<V>(current.value));
 
-      return current.value as V
+      return current.value as V;
     }
 
-    throw { message: "can not find requested provider value" }
+    throw { message: "can not find requested provider value" };
   }
 
   override createElement(): ProviderElement {
-    return new ProviderElement(this)
+    return new ProviderElement(this);
   }
 }
 
 class ProviderElement extends Element {
-  declare widget: ProviderWidget<unknown, unknown>
-  child!: Element
+  declare widget: ProviderWidget<unknown, unknown>;
+  child!: Element;
 
   get providerKey() {
-    return this.widget.providerKey
+    return this.widget.providerKey;
   }
 
   get value() {
-    return this.widget.value
+    return this.widget.value;
   }
 
   visitChildren(visitor: (child: Element) => void): void {
-    visitor(this.child)
+    visitor(this.child);
   }
 
   mount(newParent?: Element | undefined): void {
-    super.mount(newParent)
-    this.child = this.inflateWidget(this.widget.child)
+    super.mount(newParent);
+    this.child = this.inflateWidget(this.widget.child);
   }
 
   update(newWidget: Widget): void {
-      super.update(newWidget)
-      this.performRebuild()
+    super.update(newWidget);
+    this.performRebuild();
   }
 
   protected override performRebuild(): void {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      this.child = this.updateChild(this.child, this.child.widget)!
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.child = this.updateChild(this.child, this.child.widget)!;
   }
 
   constructor(widget: ProviderWidget<unknown, unknown>) {
-    super(widget)
-    this.widget = widget
+    super(widget);
+    this.widget = widget;
   }
 }
 
-export default ProviderWidget
+export default ProviderWidget;
