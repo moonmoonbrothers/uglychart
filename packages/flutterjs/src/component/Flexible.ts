c894a64329b5f30a257d189b24bb21877873be72
daeseungmoon <tmdeoans@snu.ac.kr>
2023-08-19 22:33:17 +0900

    asdf

--- packages/flutterjs/src/component/Flexible.ts
+++ packages/flutterjs/src/component/Flexible.ts
@@ -1,18 +1,19 @@
-import type Widget from "../widget/Widget"
-import _Flexible from "./base/BaseFlexible"
+import type Widget from "../widget/Widget";
+import _Flexible from "./base/BaseFlexible";
+import {} from "../../src/utils";
 
 export default function Flexible({
   child,
   flex = 1,
   fit = "loose",
 }: {
-  child?: Widget
-  flex?: number
-  fit?: "loose" | "tight"
+  child?: Widget;
+  flex?: number;
+  fit?: "loose" | "tight";
 } = {}) {
   return new _Flexible({
     child,
     fit,
     flex,
-  })
+  });
 }
