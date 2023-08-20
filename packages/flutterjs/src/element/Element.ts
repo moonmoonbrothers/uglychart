395f336e46b09ee9c68aa2bbd054e249c0a36986
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2023-08-20 19:59:20 +0900

    resolved #135 Feature 135 globalkey (#137)
    * waiting global key until all widget accept key
    
    * add global key

--- packages/flutterjs/src/element/Element.ts
+++ packages/flutterjs/src/element/Element.ts
@@ -1,6 +1,6 @@
 import type RenderObject from "../renderobject/RenderObject";
 import type { RenderContext } from "../runApp";
-import { BuildOwner, RenderOwner } from "../scheduler";
+import { BuildOwner, GlobalKey, RenderOwner } from "../scheduler";
 import Widget from "../widget/Widget";
 import RenderObjectElement from "./RenderObjectElement";
 
@@ -83,6 +83,10 @@ class Element {
       this.depth = newParent.depth + 1;
     }
     this.parent = newParent;
+
+    if (this.widget.key instanceof GlobalKey) {
+      this.buildOwner.registerGlobalKey(this.widget.key, this);
+    }
   }
 
   update(newWidget: Widget) {
