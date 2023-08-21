1a379c6a8cd413262537dbd61b37fdce8c984186
moonmoon <64200419+Moon-DaeSeung@users.noreply.github.com>
2023-08-21 15:30:24 +0900

    add mouse event on gesture detector (#140)

--- packages/flutterjs/src/component/base/BaseGestureDetector.ts
+++ packages/flutterjs/src/component/base/BaseGestureDetector.ts
@@ -5,21 +5,38 @@ import type Widget from "../../widget/Widget";
 
 class BaseGestureDetector extends SingleChildRenderObjectWidget {
   onClick?: () => void;
+  onMouseUp?: (e: MouseEvent) => void;
+  onMouseMove?: (e: MouseEvent) => void;
+  onMouseDown?: (e: MouseEvent) => void;
   constructor({
     child,
     onClick,
+    onMouseDown,
+    onMouseMove,
+    onMouseUp,
     key,
   }: {
     child?: Widget;
     onClick?: () => void;
+    onMouseUp?: (e: MouseEvent) => void;
+    onMouseMove?: (e: MouseEvent) => void;
+    onMouseDown?: (e: MouseEvent) => void;
     key?: any;
   }) {
     super({ child, key });
     this.onClick = onClick;
+    this.onMouseDown = onMouseDown;
+    this.onMouseMove = onMouseMove;
+    this.onMouseUp = onMouseUp;
   }
 
   override createRenderObject(): RenderGestureDetector {
-    return new RenderGestureDetector({ onClick: this.onClick });
+    return new RenderGestureDetector({
+      onClick: this.onClick,
+      onMouseDown: this.onMouseDown,
+      onMouseMove: this.onMouseMove,
+      onMouseUp: this.onMouseUp,
+    });
   }
 
   updateRenderObject(renderObject: RenderGestureDetector): void {
@@ -38,9 +55,49 @@ class RenderGestureDetector extends SingleChildRenderObject {
     this._onClick = prop ?? function () {};
     this.markNeedsPaint();
   }
-  constructor({ onClick = () => {} }: { onClick?: () => void }) {
+  private _onMouseDown: (e: MouseEvent) => void;
+  get onMouseDown(): (e: MouseEvent) => void {
+    return this._onMouseDown;
+  }
+  set onMouseDown(prop: ((e: MouseEvent) => void) | undefined) {
+    if (this._onMouseDown === prop) return;
+    this._onMouseDown = prop ?? function () {};
+    this.markNeedsPaint();
+  }
+  private _onMouseMove: (e: MouseEvent) => void;
+  get onMoseMove(): (e: MouseEvent) => void {
+    return this._onMouseMove;
+  }
+  set onMouseMove(prop: ((e: MouseEvent) => void) | undefined) {
+    if (this._onMouseMove === prop) return;
+    this._onMouseMove = prop ?? function () {};
+    this.markNeedsPaint();
+  }
+  private _onMouseUp: (e: MouseEvent) => void;
+  get onMouseUp(): (e: MouseEvent) => void {
+    return this._onMouseUp;
+  }
+  set onMouseUp(prop: ((e: MouseEvent) => void) | undefined) {
+    if (this._onMouseUp === prop) return;
+    this._onMouseUp = prop ?? function () {};
+    this.markNeedsPaint();
+  }
+  constructor({
+    onClick = () => {},
+    onMouseDown = () => {},
+    onMouseUp = () => {},
+    onMouseMove = () => {},
+  }: {
+    onClick?: () => void;
+    onMouseUp?: (e: MouseEvent) => void;
+    onMouseMove?: (e: MouseEvent) => void;
+    onMouseDown?: (e: MouseEvent) => void;
+  }) {
     super({ isPainter: true });
     this._onClick = onClick;
+    this._onMouseDown = onMouseDown;
+    this._onMouseMove = onMouseMove;
+    this._onMouseUp = onMouseUp;
   }
 
   protected performPaint(
