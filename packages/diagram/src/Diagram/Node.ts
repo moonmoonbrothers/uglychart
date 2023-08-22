e003313df7dd8ab0315cc8c813802b7847517df1
daeseungmoon <tmdeoans@snu.ac.kr>
2023-08-22 09:17:12 +0900

    multiple nodes

--- packages/diagram/src/Diagram/Node.ts
+++ packages/diagram/src/Diagram/Node.ts
@@ -1,169 +1,18 @@
-import {
-  Offset,
-  Widget,
-  StatefulWidget,
-  Element,
-  Column,
-  MainAxisAlignment,
-  MainAxisSize,
-  Row,
-  Transform,
-  SizedBox,
-  GlobalKey,
-  State,
-  Matrix4,
-  Stack,
-  CustomPaint,
-  Size,
-  Path,
-  Padding,
-  EdgeInsets,
-} from "@moonmoonbrothers/flutterjs";
+import { Widget } from "@moonmoonbrothers/flutterjs";
 import { functionalizeClass } from "../utils";
-import { Draggable } from "./components";
 
-class Temp extends StatefulWidget {
+export class Node {
   content: Widget;
-  childNodes: Widget[];
-  constructor({
-    key,
-    childNodes,
-    content,
-  }: {
-    key?: any;
-    content: Widget;
-    childNodes: Widget[];
-  }) {
-    super(key);
+  childNodes: Node[] = [];
+
+  constructor(content: Widget) {
     this.content = content;
-    this.childNodes = childNodes;
-  }
-  createState(): State<StatefulWidget> {
-    return new TempState();
-  }
-}
-
-class TempState extends State<Temp> {
-  vortextPosition: Offset;
-  childVortextPositions: Offset[];
-  vortexKey: GlobalKey;
-  childVortextKeys: GlobalKey[];
-
-  didChangePosition() {
-    if (this.element == null) return;
-    this.element.scheduler.addPostFrameCallbacks(() => {
-      this.setState(() => {
-        const origin = matrixToOffset(this.element.renderObject.matrix);
-        this.vortextPosition = matrixToOffset(
-          this.vortexKey.currentContext.renderObject.matrix
-        ).minus(origin);
-        this.childVortextPositions = this.childVortextKeys.map(
-          ({ currentContext }) =>
-            matrixToOffset(currentContext.renderObject.matrix).minus(origin)
-        );
-      });
-    });
-  }
-  initState(context: Element): void {
-    super.initState(context);
-    this.vortexKey = new GlobalKey();
-    this.childVortextKeys = this.widget.childNodes.map(() => new GlobalKey());
-    this.didChangePosition();
   }
 
-  build(context: Element): Widget {
-    const { content, childNodes } = this.widget;
-
-    return Stack({
-      children: [
-        CustomPaint({
-          size: Size.infinite,
-          painter: {
-            dependencies: {
-              childVortextPositions: this.childVortextPositions,
-              vortextPosition: this.vortextPosition,
-            },
-            shouldRepaint: ({ dependencies }) => {
-              if (dependencies == null) return true;
-
-              return (
-                dependencies.childVortextPositions !==
-                  this.childVortextPositions ||
-                dependencies.vortextPosition !== this.vortextPosition
-              );
-            },
-            createDefaultSvgEl(context) {
-              return {
-                path: context.createSvgEl("path"),
-              };
-            },
-            paint: ({ path }, size) => {
-              if (
-                this.childVortextPositions == null ||
-                this.vortextPosition == null
-              )
-                return;
-
-              const painter = new Path();
-              this.childVortextPositions.forEach((childPosition) => {
-                painter.moveTo(this.vortextPosition).lineTo(childPosition);
-              });
-              path.setAttribute("stroke-width", "2");
-              path.setAttribute("stroke", "black");
-              path.setAttribute("d", painter.getD());
-            },
-          },
-        }),
-        Column({
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Draggable({
-              onDrag: () => {
-                this.didChangePosition();
-              },
-              child: Column({
-                mainAxisSize: MainAxisSize.min,
-                children: [content, Vortex({ key: this.vortexKey })],
-              }),
-            }),
-            SizedBox({ height: 30 }),
-            Row({
-              mainAxisSize: MainAxisSize.min,
-              children: this.childVortextKeys.map((key, i) =>
-                Draggable({
-                  onDrag: () => {
-                    this.didChangePosition();
-                  },
-                  child: Column({
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Vortex({ key }),
-                      Padding({
-                        padding: EdgeInsets.symmetric({ horizontal: 10 }),
-                        child: childNodes[i],
-                      }),
-                    ],
-                  }),
-                })
-              ),
-            }),
-          ],
-        }),
-      ],
-    });
+  addChildNode(node: Node) {
+    this.childNodes.push(node);
+    return this;
   }
 }
 
-function Vortex({ key }: { key: GlobalKey }) {
-  return SizedBox({ width: 0, height: 0, key });
-}
-
-function matrixToOffset(matrix: Matrix4) {
-  return new Offset({
-    x: matrix.storage[12],
-    y: matrix.storage[13],
-  });
-}
-
-export default functionalizeClass(Temp);
+export default functionalizeClass(Node);
--- packages/storybook-diagram/src/stories/DIagram/example/Basic.ts
+++ packages/storybook-diagram/src/stories/DIagram/example/Basic.ts
@@ -1,32 +1,74 @@
 import { dedent } from 'ts-dedent';
-import { Node } from '@moonmoonbrothers/diagram';
+import { Diagram, Node } from '@moonmoonbrothers/diagram';
 import { Container } from '@moonmoonbrothers/flutterjs';
 
+const child4 = Node(
+	Container({
+		width: 300,
+		height: 50,
+		color: 'black'
+	})
+);
+const child5 = Node(
+	Container({
+		width: 30,
+		height: 80,
+		color: 'orange'
+	})
+);
+const child6 = Node(
+	Container({
+		width: 60,
+		height: 80,
+		color: 'gray'
+	})
+);
+const child7 = Node(
+	Container({
+		width: 30,
+		height: 80,
+		color: 'purple'
+	})
+);
+const child1 = Node(
+	Container({
+		width: 70,
+		height: 50,
+		color: 'blue'
+	})
+);
+
+const child2 = Node(
+	Container({
+		width: 50,
+		height: 150,
+		color: 'green'
+	})
+);
+const child3 = Node(
+	Container({
+		width: 30,
+		height: 100,
+		color: 'yellow'
+	})
+);
+const node = Node(
+	Container({
+		width: 50,
+		height: 100,
+		color: 'red'
+	})
+)
+	.addChildNode(child1)
+	.addChildNode(child2)
+	.addChildNode(child3);
+child1.addChildNode(child4);
+child1.addChildNode(child5);
+child3.addChildNode(child6);
+child6.addChildNode(child7);
+
 const BasicStory = {
-	widget: Node({
-		content: Container({
-			width: 50,
-			height: 100,
-			color: 'red'
-		}),
-		childNodes: [
-			Container({
-				width: 70,
-				height: 50,
-				color: 'blue'
-			}),
-			Container({
-				width: 50,
-				height: 150,
-				color: 'green'
-			}),
-			Container({
-				width: 30,
-				height: 100,
-				color: 'yellow'
-			})
-		]
-	}),
+	widget: Diagram({ node }),
 	code: dedent`
 	hi
 }`
