dbf1f17778b1b2cf996d569e6facf0ad24fe1b4b
daeseungmoon <tmdeoans@snu.ac.kr>
2023-08-23 10:36:56 +0900

    add animation on scatter chart

--- packages/uglychart/src/charts/ScatterChart/components/default/Scatter.ts
+++ packages/uglychart/src/charts/ScatterChart/components/default/Scatter.ts
@@ -4,10 +4,78 @@ import {
   Path,
   Rect,
   Size,
+  State,
+  StatefulWidget,
+  Widget,
+  Animation,
+  AnimationController,
+  Tween,
+  CurvedAnimation,
+  Transform,
+  Element,
+  BuildContext,
 } from "@moonmoonbrothers/flutterjs";
 import { Scale } from "../../types";
+import { funcionalizeClass } from "../../../../utils";
 
-export default function Scatter({
+class Scatter extends StatefulWidget {
+  dotRadius: number;
+  color: string;
+  points: { x: number; y: number }[];
+  scale: Scale;
+  constructor({
+    color,
+    points,
+    dotRadius,
+    scale,
+  }: {
+    dotRadius: number;
+    color: string;
+    points: { x: number; y: number }[];
+    scale: Scale;
+  }) {
+    super();
+    this.color = color;
+    this.points = points;
+    this.dotRadius = dotRadius;
+    this.scale = scale;
+  }
+
+  createState(): State<StatefulWidget> {
+    return new ScatterState();
+  }
+}
+
+class ScatterState extends State<Scatter> {
+  animationController: AnimationController;
+  tweenAnimation: Animation<number>;
+  initState(context: BuildContext): void {
+    this.animationController = new AnimationController({ duration: 200 });
+    this.animationController.addListener(() => {
+      this.setState();
+    });
+    const tween: Tween<number> = new Tween({ begin: 0, end: 1 });
+    this.tweenAnimation = tween.animated(
+      new CurvedAnimation({ parent: this.animationController })
+    );
+    this.animationController.forward();
+  }
+
+  build(context: Element): Widget {
+    const { color, points, dotRadius, scale } = this.widget;
+    return Transform.scale({
+      scale: this.tweenAnimation.value,
+      child: _Scatter({
+        color,
+        points,
+        dotRadius,
+        scale,
+      }),
+    });
+  }
+}
+
+function _Scatter({
   color,
   points,
   dotRadius,
@@ -50,3 +118,5 @@ export default function Scatter({
     },
   });
 }
+
+export default funcionalizeClass(Scatter);
