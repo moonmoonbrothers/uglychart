d2275470b3c583168b89a8ba353d5e3209097614
daeseungmoon <tmdeoans@snu.ac.kr>
2023-08-29 17:43:39 +0900

    upgrade spline shape

--- packages/uglychart/src/charts/LineChart/components/default/Line.ts
+++ packages/uglychart/src/charts/LineChart/components/default/Line.ts
@@ -56,7 +56,10 @@ export default function Line({
   });
 }
 
-function drawLine(points: { x: number; y: number }[], path: Path) {
+type BezierPoint = Point & { controlPoint: { prev: Point; next: Point } };
+type Point = { x: number; y: number };
+
+function drawLine(points: Point[], path: Path) {
   points.forEach((point, i) => {
     if (i === 0) {
       path.moveTo(point);
@@ -67,22 +70,83 @@ function drawLine(points: { x: number; y: number }[], path: Path) {
   });
 }
 
-function drawSpline(data: { x: number; y: number }[], path: Path) {
-  path.moveTo({ x: data[0].x, y: data[0].y });
-  for (let i = 1; i < data.length; i++) {
-    path.cubicTo({
-      startControlPoint: {
-        x: data[i - 1].x + (data[i].x - data[i - 1].x) / 2,
-        y: data[i - 1].y,
-      },
-      endControlPoint: {
-        x: data[i - 1].x + (data[i].x - data[i - 1].x) / 2,
-        y: data[i].y,
-      },
-      endPoint: {
-        x: data[i].x,
-        y: data[i].y,
-      },
-    });
+function drawSpline(points: Point[], path: Path) {
+  const bezierPoints = points as BezierPoint[];
+  setSplineControlPoint(bezierPoints);
+  bezierPoints.forEach((point, idx) => {
+    if (idx === 0) {
+      path.moveTo(point);
+      return;
+    }
+
+    if (point.controlPoint && bezierPoints[idx - 1]?.controlPoint?.next) {
+      const { x: prevX, y: prevY } = bezierPoints[idx - 1]!.controlPoint!.next;
+      const { controlPoint, x, y } = point;
+
+      path.cubicTo({
+        startControlPoint: {
+          x: prevX,
+          y: prevY,
+        },
+        endControlPoint: {
+          x: controlPoint.prev.x,
+          y: controlPoint.prev.y,
+        },
+        endPoint: {
+          x,
+          y,
+        },
+      });
+    }
+  });
+}
+
+function setSplineControlPoint(points: (BezierPoint | null)[]) {
+  for (
+    let i = 0, pointsSize = points.length, prev = points[0];
+    i < pointsSize;
+    i += 1
+  ) {
+    const point = points[i];
+    if (point == null) {
+      prev = points[i + 1];
+      continue;
+    }
+
+    const next = points[Math.min(i + 1, pointsSize - 1) % pointsSize];
+    if (prev && next) {
+      point.controlPoint = getControlPoints(prev, point, next);
+    }
+
+    prev = point;
   }
+}
+function getControlPoints(
+  prev: BezierPoint,
+  cur: BezierPoint,
+  next: BezierPoint
+) {
+  // http://scaledinnovation.com/analytics/splines/aboutSplines.html
+  const TENSION = 0.333;
+  const { x: x0, y: y0 } = prev;
+  const { x: x1, y: y1 } = cur;
+  const { x: x2, y: y2 } = next;
+
+  const d12 = getDistance(next, cur);
+  const d01 = getDistance(cur, prev);
+
+  const fa = (TENSION * d01) / (d01 + d12) || 0; // scaling factor for triangle Ta
+  const fb = (TENSION * d12) / (d01 + d12) || 0; // ditto for Tb, simplifies to fb=t-fa
+
+  return {
+    prev: {
+      x: x1 - fa * (x2 - x0), // x2-x0 is the width of triangle T
+      y: y1 - fa * (y2 - y0), // y2-y0 is the height of T
+    },
+    next: { x: x1 + fb * (x2 - x0), y: y1 + fb * (y2 - y0) },
+  };
+}
+
+export function getDistance(point1: Point, point2: Point) {
+  return Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2);
 }
